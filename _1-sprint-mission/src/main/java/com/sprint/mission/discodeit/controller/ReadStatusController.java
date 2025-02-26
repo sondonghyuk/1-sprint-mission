c971ebf8950088638076ec592b45a62db88ceda5
Son DongHyuk <sond5510@naver.com>
2025-02-26 10:21:29 +0900

    Update ReadStatusController

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -49,7 +50,7 @@ public class ReadStatusController implements ReadStatusApiDocs {
 
 
   //메시지 수신 정보 수정
-  @PutMapping("/{readStatusId}")
+  @PatchMapping("/{readStatusId}")
   @Override
   public ResponseEntity<ReadStatus> update(@PathVariable UUID readStatusId,
       @Valid @RequestBody ReadStatusUpdateDto readStatusUpdateDto) {
