c28c711aedb10db1940b29838927cd8a74ba6a57
Son DongHyuk <sond5510@naver.com>
2025-02-26 10:29:01 +0900

    Update RestControllerAdvie and SwaggerConfig

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/config/SwaggerConfig.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.sprint.mission.discodeit.config;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +19,10 @@ public class SwaggerConfig {
         .info(new Info()
             .title("Discodeit API 문서")
             .version("1.0")
-            .description("Discodeit 프로젝트의 Swagger API 문서입니다."));
+            .description("Discodeit 프로젝트의 Swagger API 문서입니다."))
+        .servers(List.of(
+            new Server().url("http://localhost:8080").description("로컬 서버")
+        ));
   }
 
   @Bean
--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -8,28 +8,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-@ResponseBody
+@RestControllerAdvice(basePackages = {"com.sprint.mission.discodeit.controller.api"})
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleException(NoSuchElementException e){
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
-    }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleException(IllegalArgumentException e){
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-    }
+  @ExceptionHandler(NoSuchElementException.class)
+  public ResponseEntity<String> handleException(NoSuchElementException e) {
+    return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(e.getMessage());
+  }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e){
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(e.getMessage());
-    }
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<String> handleException(IllegalArgumentException e) {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(e.getMessage());
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(e.getMessage());
+  }
 }
