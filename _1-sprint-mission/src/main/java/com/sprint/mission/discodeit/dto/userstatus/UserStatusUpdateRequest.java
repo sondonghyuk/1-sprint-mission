f17e0c8c237e356e70987c31972ff59923a1f92f
Son DongHyuk <sond5510@naver.com>
2025-03-07 13:25:37 +0900

    Update Dto

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/userstatus/UserStatusUpdateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/userstatus/UserStatusUpdateRequest.java
@@ -3,7 +3,7 @@ package com.sprint.mission.discodeit.dto.userstatus;
 import java.time.Instant;
 
 public record UserStatusUpdateRequest(
-    Instant newLastConnectTime
+    Instant newLastActiveAt
 ) {
 
 }
