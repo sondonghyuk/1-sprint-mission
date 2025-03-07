947f8ca47252747caa632e1782bb564abeca106f
Son DongHyuk <sond5510@naver.com>
2025-03-07 13:23:29 +0900

    Update Dto

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/readstatus/ReadStatusUpdateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/readstatus/ReadStatusUpdateRequest.java
@@ -6,8 +6,6 @@ import java.util.UUID;
 
 @Schema(description = "수정할 읽음 상태 정보")
 public record ReadStatusUpdateRequest(
-    UUID userId,
-    UUID channelId,
     Instant newLastReadAt
 ) {
 
--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/user/UserDto.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/user/UserDto.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit.dto.user;
 import java.time.Instant;
 import java.util.UUID;
 
-//조회
 public record UserDto(
     UUID id,
     Instant createdAt,
