71d2155c30c6b56d301b7ed4d910ad88b81d5db2
Son DongHyuk <sond5510@naver.com>
2025-03-28 13:39:34 +0900

    refactor : ReadStatusEntity

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -14,30 +14,32 @@ import java.io.Serializable;
 import java.time.Instant;
 import lombok.NoArgsConstructor;
 
-@Getter
 @Entity
+@Table(
+    name = "read_statuses",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "channel_id"
+        })
+    }
+)
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "read_statuses", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id",
-    "channel_id"}))
 //사용자가 채널 별 마지막으로 메시지를 읽은 시간을 표현하는 도메인 모델
 //사용자별 각 채널에 읽지 않은 메시지를 확인하기 위해 활용
-public class ReadStatus extends BaseUpdatableEntity implements Serializable {
+public class ReadStatus extends BaseUpdatableEntity {
 
-  private static final long serialVersionUID = 1L; //직렬화 버전
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "user_id", columnDefinition = "uuid")
   private User user;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "channel_id", nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "channel_id", columnDefinition = "uuid")
   private Channel channel;
 
-  @Column(name = "last_read_at", nullable = false)
+  @Column(columnDefinition = "timestamp with time zone", nullable = false)
   private Instant lastReadAt; // 마지막으로 읽은 시간
 
   public ReadStatus(User user, Channel channel, Instant lastReadAt) {
-    super();
     this.user = user;
     this.channel = channel;
     this.lastReadAt = lastReadAt;
