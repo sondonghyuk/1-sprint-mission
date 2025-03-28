e6f3d79e1dcc8e3bc37ad7d3785fdef8114ca42d
Son DongHyuk <sond5510@naver.com>
2025-03-28 13:13:49 +0900

    refactor : BinaryContentEntity

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -12,14 +12,12 @@ import java.io.Serializable;
 
 @Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "binary_contents")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 //바이너리 데이터(이미지,파일)를 표현하는 도메인 모델
 //사용자의 프로필 이미지,메시지에 첨부된 파일을 저장하기 위해 활용
-public class BinaryContent extends BaseEntity implements Serializable {
+public class BinaryContent extends BaseEntity {
 
-  private static final long serialVersionUID = 1L;
-  //메타 정보
   @Column(nullable = false)
   private String fileName; // 파일명
 
@@ -30,7 +28,6 @@ public class BinaryContent extends BaseEntity implements Serializable {
   private String contentType; // 타입
 
   public BinaryContent(String fileName, Long size, String contentType) {
-    super();
     this.fileName = fileName;
     this.size = size;
     this.contentType = contentType;
