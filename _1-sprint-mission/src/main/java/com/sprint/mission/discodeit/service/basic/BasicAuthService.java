f74f0b25d13f8d09be43aa2729e5be55997ade2a
Son DongHyuk <sond5510@naver.com>
2025-03-28 15:32:03 +0900

    refactor : BasicAuthService

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -6,25 +6,27 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
-import com.sprint.mission.discodeit.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
 
   private final UserRepository userRepository;
-  private UserMapper userMapper;
+  private final UserMapper userMapper;
 
+  @Transactional(readOnly = true)
   @Override
   public UserDto login(LoginRequest loginRequest) {
     User user = userRepository.findByUsername(loginRequest.username())
-        .orElseThrow(() -> new NoSuchElementException("user not found"));
+        .orElseThrow(
+            () -> new NoSuchElementException("User with username \" + username + \" not found"));
     if (!user.getPassword().equals(loginRequest.password())) {
-      throw new NoSuchElementException("Username or password is incorrect");
+      throw new NoSuchElementException("password is incorrect");
     }
     return userMapper.toDto(user);
   }
