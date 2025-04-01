39230d71217fb92eb70af06c315a622ac7b47ff3
Son DongHyuk <sond5510@naver.com>
2025-04-01 11:08:08 +0900

    refactor : channel request dto

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/channel/PublicChannelCreateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/channel/PublicChannelCreateRequest.java
@@ -1,14 +1,15 @@
 package com.sprint.mission.discodeit.dto.channel;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record PublicChannelCreateRequest(
-    @NotNull
+    @NotBlank(message = "채널 이름은 필수입니다.")
     String name,
 
-    @NotNull
-    @Size(max = 100)
+    @NotBlank(message = "채널 설명은 필수입니다.")
+    @Size(max = 100, message = "최대 100자까지 가능합니다.")
     String description
 ) {
 
--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/channel/PublicChannelUpdateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/channel/PublicChannelUpdateRequest.java
@@ -1,16 +1,17 @@
 package com.sprint.mission.discodeit.dto.channel;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Schema(description = "수정할 Channel 정보")
 public record PublicChannelUpdateRequest(
-    @NotNull
+    @NotBlank(message = "채널 이름은 필수입니다.")
     String newName,
 
-    @NotNull
-    @Size(max = 100)
+    @NotBlank(message = "채널 설명은 필수입니다.")
+    @Size(max = 100, message = "최대 100자까지 가능합니다.")
     String newDescription
 ) {
 
