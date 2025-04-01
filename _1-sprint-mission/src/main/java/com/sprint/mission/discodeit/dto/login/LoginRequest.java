11950f61cd47e74d2ae6d51e3e0147705e99e64c
Son DongHyuk <sond5510@naver.com>
2025-04-01 11:10:07 +0900

    refactor : login request dto

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/login/LoginRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/login/LoginRequest.java
@@ -2,14 +2,15 @@ package com.sprint.mission.discodeit.dto.login;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "로그인 정보")
 public record LoginRequest(
-    @NotNull
+    @NotBlank(message = "username 은 필수입니다.")
     String username,
 
-    @NotNull
+    @NotBlank(message = "password 는 필수입니다.")
     String password
 ) {
 
