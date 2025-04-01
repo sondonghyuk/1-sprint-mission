24af3388739ec1836fef9ff90d10cdd33cbb640c
Son DongHyuk <sond5510@naver.com>
2025-04-01 10:49:17 +0900

    refactor : user request dto

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/user/UserCreateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/user/UserCreateRequest.java
@@ -1,20 +1,23 @@
 package com.sprint.mission.discodeit.dto.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
-    @NotNull
+    @NotBlank(message = "이름은 필수입니다.")
     @Size(min = 1, max = 20)
     String username,
 
     @Email
-    @NotNull
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
     String email,
 
     @NotNull
-    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,}$", message = "비밀번호는 최소 8자이며 숫자, 문자, 특수문자를 포함해야 합니다.")
     String password
 ) {
 
