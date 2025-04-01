cfdff7156756e0ecb29f62257ef282148203fb76
Son DongHyuk <sond5510@naver.com>
2025-04-01 11:11:05 +0900

    refactor : message request dto

--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/message/MessageCreateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/message/MessageCreateRequest.java
@@ -1,12 +1,13 @@
 package com.sprint.mission.discodeit.dto.message;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Schema(description = "Message 생성 정보")
 public record MessageCreateRequest(
-    @NotNull
+    @NotBlank(message = "메시지 내용은 필수입니다.")
     String content,
 
     @NotNull
--- _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/message/MessageUpdateRequest.java
+++ _1-sprint-mission/src/main/java/com/sprint/mission/discodeit/dto/message/MessageUpdateRequest.java
@@ -2,12 +2,13 @@ package com.sprint.mission.discodeit.dto.message;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "수정할 Message 내용")
 public record MessageUpdateRequest(
 
-    @NotNull
+    @NotBlank(message = "메시지 내용은 필수입니다.")
     String newContent
 ) {
 
